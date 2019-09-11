5da4c20250fe1c581b8f0c3169c4256339014a94
kelvin <kigbinedion@ivytech.edu>
2019-09-11 15:15:58 -0400

    SECOND COMMIT

--- M01/Homework1/src/Main.java
+++ M01/Homework1/src/Main.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int v = input.nextInt();
+        System.out.println("You entered " + v);
+
+        System.out.print("Enter a double: ");
+        double v2 = input.nextDouble();
+        System.out.println("You entered " + v2);
+
+    }
 
 }
